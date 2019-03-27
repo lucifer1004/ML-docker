c19783dd6795d9b7b37b995643aaea03edbb79ad
Gabriel Wu <qqbbnease1004@126.com>
2019-03-27 16:09:32 +0800

    fix: NotebookApp.ip

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ c.IPKernelApp.pylab = 'inline'
 c.NotebookApp.certfile = './mycert.pem'
 # 给出刚刚创建的密码的哈希值
 c.NotebookApp.password = 'sha1:b443380803d1:d57b93914df5a23d03a48254d801d497e2b8e945'
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 #  给出运行的端口，ipython默认为8888
 c.NotebookApp.port = 8888
 #  禁止在运行ipython的同时弹出浏览器
