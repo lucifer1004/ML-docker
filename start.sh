33e798526ca00e1200274c598123e649637fc309
Gabriel Wu <wuzihua@pku.edu.cn>
2019-04-11 08:55:11 +0800

    update workspace

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
 export UNAME=gabriel
+export LOCAL_WORKSPACE="/home/ubuntu/com.github/lucifer1004/ProSAIL"
 
 docker rm jupyter -f && \
-docker run --name jupyter -p 11515:8888 --restart always -v /home/ubuntu/workspace:/home/${UNAME}/workspace lucifer1004/machine-learning
+docker run --name jupyter -p 11515:8888 --restart always -v ${LOCAL_WORKSPACE}:/home/${UNAME}/workspace lucifer1004/machine-learning
